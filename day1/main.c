0e09f09e271d623cf2a10becdf4ee84b6bcf527f
Hectonight <joshuaoring@yahoo.com>
2026-08-14 15:41:27 -0400

    first commit

---
+++ day1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int count = 0;
+    unsigned char pos = 50;
+    char dir;
+    unsigned int n;
+    while (scanf("%c%u\n", &dir, &n) != EOF) {
+        n %= 100;
+        switch (dir) {
+            case 'R':
+                pos += n;
+                if (pos >= 100) {
+                    pos -= 100;
+                }
+                break;
+            case 'L':
+                if (pos < n) {
+                    pos += 100;
+                }
+                pos -= n;
+        }
+        count += pos == 0;
+    }
+
+    printf("%d\n", count);
+}
+
